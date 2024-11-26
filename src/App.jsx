f318d867917d7e1ac700b2a3b55f8c5a1412d021
jainaastha22 <137791063+jainaastha22@users.noreply.github.com>
2024-11-26 11:06:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import About from "./components/About"
+import Contact from "./components/Contact"
+import Experience from "./components/Experience"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
 import Technologies from "./components/Technologies"
 
 const App = () => {
@@ -21,7 +24,7 @@ const App = () => {
       <Contact />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App
