778117e71424e58eb71fbd708dc225bde63d77f0
jainaastha22 <137791063+jainaastha22@users.noreply.github.com>
2024-11-26 11:26:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,43 @@ const Navbar = () => {
             <FaInstagram />
         </div>
     </nav>;
-}
+}import logo from "../assets/Logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (<nav className=" mb-20 flex items-center justify-between py-6 relative z-20">
+        <div className="flex flex-shrink-0 items-center">
+            <img src={logo} alt="logo" className="h-8 w-8"/>
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl text-gray-800">
+        <a 
+          href="https://www.linkedin.com/in/aastha-jain22/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+            <FaLinkedin />
+        </a>
+        <a 
+          href="https://github.com/jainaastha22" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a 
+          href="https://x.com/Aastha_019" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <FaSquareXTwitter />
+        </a>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar
 
 export default Navbar
